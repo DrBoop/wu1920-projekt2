71d68aa6be9044b506ccce7940d82944294411e4
DrBoop <philip.dahlgren@elev.ga.ntig.se>
2020-02-18 19:14:07 +0100

    Lektion v.6

--- js/script.js
+++ js/script.js
@@ -1,18 +1,5 @@
-document.getElementById('ham-button').addEventListener('click', function () {
-    toggleClass(document.getElementById('ham-menu'), "active")
+document.querySelector('.button').addEventListener('click', function () {
+
+    document.querySelector('.button').classList.toggle('change');
+    document.querySelector('.menu').classList.toggle('active');
 });
-
-document.getElementById('close').addEventListener('click', function () {
-    toggleClass(document.getElementById('ham-menu'), "active")
-});
-
-
-function toggleClass(element, className) {
-    var classString = element.className, nameIndex = classString.indexOf(className);
-    if (nameIndex == -1) {
-        classString += ' ' + className;
-    } else {
-        classString = classString.substr(0, nameIndex) + classString.substr(nameIndex + className.length);
-    }
-    element.className = classString;
-}
